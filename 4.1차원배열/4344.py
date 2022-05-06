48b8d0dcd6eafa3ae841267d27db6d76f1d067c9
AI_09_김혜수 <94281700+hyesukim1@users.noreply.github.com>
2022-05-06 09:44:25 +0900

    백준 문제 풀이

--- 4.1차원배열/4344.py
+++ 4.1차원배열/4344.py
@@ -1,11 +1,13 @@
 # 내일 다시 풀기
 # 평균은 넘겠지
-
 n = int(input())
 
 for i in range(n):
-    score_list = list(map(int, input()))
-    for j in score_list[1:-1]:
-        
-    score_list[0]
-    
+    score_list = list(map(int, input().split()))
+    avg = sum(score_list[1:])/score_list[0]
+    cnt = 0
+    for j in score_list[1:]:
+        if j > avg:
+            cnt+=1
+    rate = cnt/score_list[0] * 100
+    print(f'{rate:.3f}%')
