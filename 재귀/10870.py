59cd1f18a8bc4ef1b5f3ddd6fc4e4a4916dbcfc2
AI_09_김혜수 <94281700+hyesukim1@users.noreply.github.com>
2022-07-04 22:11:17 +0900

    Update 10870.py

--- 재귀/10870.py
+++ 재귀/10870.py
@@ -3,12 +3,9 @@
 # n = int(input())
 
 def fibo(n):
-    if n == 0:
-        return 0
-    elif n == 1 or n == 2:
-        return 1
-    else:
-        return fibo(n-1)+fibo(n-2)
-    
-ans = fibo(10)
-print(ans)
+    if n <=1:
+        return n
+    return fibo(n-1)+fibo(n-2)
+
+num = int(input())
+print(fibo(num))
