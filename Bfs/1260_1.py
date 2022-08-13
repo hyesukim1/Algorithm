8acc596577d403bd0c327ef2d54b0910dde83e49
AI_09_김혜수 <94281700+hyesukim1@users.noreply.github.com>
2022-08-13 13:11:25 +0900

    22.08.13

---
+++ Bfs/1260_1.py
@@ -0,0 +1,36 @@
+from collections import deque
+import sys
+read = sys.stdin.readline
+
+def bfs(v):
+    q = deque()
+    q.append(v)
+    visited[v] = 1
+    while q:
+        v = q.popleft()
+        print(v, end=" ")
+        for i in range(1, node+1):
+            if visited[i] == 0 and graph[v][i] == 1:
+                q.append(i)
+                visited[i] = 1
+                
+                
+def dfs(v):
+    visited2[v] = 1
+    print(v, end=" ")
+    for i in range(1, node+1):
+        if visited2[i] == 0 and graph[v][i] == 1:
+            dfs(i)
+
+node, edge, start = map(int, read().split())
+graph = [[0]*(node+1) for _ in range(node+1)]
+visited = [0] * (node+1)
+visited2 = [0] * (node+1)
+
+for _ in range(edge):
+    a, b = map(int, read().split())
+    graph[a][b] = graph[b][a] = 1
+
+dfs(start)
+print()
+bfs(start)
