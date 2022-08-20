4a31c86c0a24d9bbbd86286ebb3dd1f4a70212e0
AI_09_김혜수 <94281700+hyesukim1@users.noreply.github.com>
2022-08-20 19:24:28 +0900

    22.08.20

--- Bfs/1012.py
+++ Bfs/1012.py
@@ -1,9 +1,40 @@
 # 유기농 배추
 
 test_case = int(input())
-m, n, k = map(int, input().split(' '))
-graph = [a for a in range(map(int, input()))]
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
-for i in range(m):
-    for j in range(n):
-        if graph[i][j] == 1:
+
+def BFS(x, y):
+    queue = [(x, y)]
+    graph[x][y] = 0
+    
+    while queue:
+        x, y = queue.pop(0)
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if nx < 0 or nx >= m or ny < 0 or ny >= n:
+                continue
+            
+            if graph[nx][ny] == 1:
+                queue.append((nx, ny))
+                graph[nx][ny] = 0
+
+for i in range(test_case):
+    m, n, k = map(int, input().split())
+    graph = [[0]*n for _ in range(m)]
+    cnt = 0
+    
+    for j in range(k):
+        x, y = map(int, input().split())
+        graph[x][y] = 1
+        
+    for a in range(m):
+        for b in range(n):
+            if graph[a][b] == 1:
+                BFS(a, b)
+                cnt +=1
+    print(cnt)
