7c5f4f7d1e14a02016b3ea115824185847e3f90b
AI_09_김혜수 <94281700+hyesukim1@users.noreply.github.com>
2022-09-19 21:33:58 +0900

    Update 2108.py

--- 9.정렬/2108.py
+++ 9.정렬/2108.py
@@ -2,7 +2,33 @@
 
 n = int(input())
 
+list = []
+
 for i in range(n):
-    num = input().split()
+    num = int(input())
+    list.append(num)
+
+# 평균
+print(sum(list)/len(list))
+
+# 중앙값
+
+srt = list.sort()
+idx = 0
+median = 0
+
+if len(list)%2 == 0:
+    idx = len(list) // 2
+    median = (list[idx-1] + list[idx])/2
+else:
+    idx = len(list) // 2 + 1
+    median = list[idx]
     
+print(median)
+
+# 최빈값
+
+
+# 범위
+
 
